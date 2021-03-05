f01b53ed2b020bf9c960d06d99723d202637ec21
Jasmine Mai <Jasmine.W.Mai.20@dartmouth.edu>
2021-03-05 10:12:36 -0800

    fix 6 hour typo

--- event/parsing/metrics.go
+++ event/parsing/metrics.go
@@ -45,7 +45,7 @@ func ProvideEventMetrics() fx.Option {
 			prometheus.HistogramOpts{
 				Name:    "boot_time_duration",
 				Help:    "tracks boot time durations in s",
-				Buckets: []float64{60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 900, 1200, 1500, 1800, 3600, 7200, 14400, 201600},
+				Buckets: []float64{60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 900, 1200, 1500, 1800, 3600, 7200, 14400, 21600},
 			},
 			FirmwareLabel,
 			HardwareLabel,
@@ -54,7 +54,7 @@ func ProvideEventMetrics() fx.Option {
 			prometheus.HistogramOpts{
 				Name:    "reboot_to_manageable_duration",
 				Help:    "tracks total boot time (reboot-pending to fully-manageable) durations in s",
-				Buckets: []float64{60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 900, 1200, 1500, 1800, 3600, 7200, 14400, 201600},
+				Buckets: []float64{60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 900, 1200, 1500, 1800, 3600, 7200, 14400, 21600},
 			},
 			FirmwareLabel,
 			HardwareLabel,
